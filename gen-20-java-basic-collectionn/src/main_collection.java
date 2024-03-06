00f0336c0298bf6f89f05cbbff89d73c2f8b2cc8
Muhammad Fatham M A <fathamubina1@gmail.com>
2024-03-06 11:30:29 +0700

    Update tugas java basic collection

--- gen-20-java-basic-collectionn/src/main_collection.java
+++ gen-20-java-basic-collectionn/src/main_collection.java
@@ -1,33 +1,33 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Map;
+import java.util.*;
 
-public class main_collection {
+public class Main_collection {
     private List<String> dataList;
     private Map<Integer, String> dataMap;
     private Scanner scanner;
 
     //Konstruktor
-    public main_collection() {
-        this.dataList = new ArrayList<>();
-        this.dataMap = new HashMap<>();
+    public Main_collection() {
+        this.dataList = new ArrayList<>();//ArrayList untuk nama
+        this.dataMap = new HashMap<>();//HashMap untuk nama dan ID
         this.scanner = new Scanner(System.in);
     }
 
     //Method menambah nama
     public void addDataToList(String data) {
-        dataList.add(data);
+        dataList.add(data);//manambahkan string nama
     }
 
     //Method menambahkan ID ke Nama
     public void convertListToMap() {
         for (int i = 0; i < dataList.size(); i++) {
-            System.out.print("Masukkan id untuk nama \"" + dataList.get(i) + "\": ");
-            int key = scanner.nextInt();
-            scanner.nextLine(); // Consume newline
-            dataMap.put(key, dataList.get(i));
+            try {
+                System.out.print("Masukkan id untuk nama \"" + dataList.get(i) + "\": ");
+                int key = scanner.nextInt();
+                dataMap.put(key, dataList.get(i));//Menambahkan id(key) kedalam dataList menggunakan hashmap
+            } catch (InputMismatchException e){//Exception jika user input non integer
+                System.err.println("Input id harus berupa bilangan bulat");
+                scanner.nextLine();
+            }
         }
     }
 
@@ -35,26 +35,36 @@ public class main_collection {
     public void displayDataList() {
         System.out.println("Data Nama :");
         for (int i = 0; i < dataList.size(); i++) {
-            System.out.println(dataList.get(i));
+            System.out.println(dataList.get(i));//Menampilkan nama-nama dalam data list
         }
     }
 
+    //Method display Map (ID, Nama)
     public void displayDataMap() {
         System.out.println("Data Nama & ID :");
-        for (Map.Entry<Integer, String> entry : dataMap.entrySet()) {
-            System.out.println("ID: " + entry.getKey() + ", Nama: " + entry.getValue());
+        for (Map.Entry<Integer, String> entry : dataMap.entrySet()) {// for each tiap entriSet dalam dataMap, dengan menggunakan obj entry
+            System.out.println("ID: " + entry.getKey() + ", Nama: " + entry.getValue());//Menampilkan id(key) dan nama secara urut dari id(key) terkecil
         }
     }
 
-    public void displayDataByKey(int key) {
-        if (dataMap.containsKey(key)) {
-            System.out.println("Data dengan key " + key + ": " + dataMap.get(key));
-        } else {
-            System.out.println("Data dengan key " + key + " tidak ditemukan.");
+    //Method display name by id(key)
+    public void displayDataByKey() {
+        try {
+            System.out.print("Masukkan nomor key untuk menampilkan data: ");
+            int key = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+            if (dataMap.containsKey(key)) {//Jika dalam DataMap terdapat id (key)
+                System.out.println("Data dengan ID " + key + ": " + dataMap.get(key));// Menampilkan nama dengan ID
+            } else {//Jika data tidak ditemukan
+                System.out.println("Data dengan ID " + key + " tidak ditemukan.");
+            }
+        } catch (InputMismatchException e){//Exception jika user input non integer
+            System.err.println("Input id harus berupa bilangan bulat");
+            scanner.nextLine();
         }
     }
 
-    public void displayMenu() {
+    public void displayMenu() {// method display menu
         System.out.println("\nMenu:");
         System.out.println("1. Tambahkan Nama");
         System.out.println("2. Tambahkan ID");
@@ -79,13 +89,10 @@ public class main_collection {
                 displayDataMap();
                 break;
             case 5:
-                System.out.print("Masukkan nomor key untuk menampilkan data: ");
-                int selectedKey = scanner.nextInt();
-                scanner.nextLine(); // Consume newline
-                displayDataByKey(selectedKey);
+                displayDataByKey();
                 break;
             case 6:
-                System.out.println("Terima kasih! Keluar dari program.");
+                System.out.println("Program selesai");
                 scanner.close();
                 System.exit(0);
             default:
@@ -93,27 +100,35 @@ public class main_collection {
         }
     }
 
+    //Method menambahkan data Nama
     public void addDataToListWithQ() {
         System.out.println("Masukkan Nama, ketik 'q' untuk berhenti:");
         while (true) {
             System.out.print("Nama : ");
             String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("q")) {
+            if (input.equalsIgnoreCase("q")) {//Jika user memasukan case 'q' maka break
                 break;
             }
-            addDataToList(input);
+            addDataToList(input);//Memanggil method add data nama berdasarkan inout string user
         }
     }
 
+    //Main method
     public static void main(String[] args) {
-        main_collection converter = new main_collection();
+        Main_collection data1 = new Main_collection();//new obj
 
         while (true) {
-            converter.displayMenu();
-            System.out.print("Masukkan pilihan: ");
-            int choice = converter.scanner.nextInt();
-            converter.scanner.nextLine(); // Consume newline
-            converter.handleUserInput(choice);
+            try {
+                data1.displayMenu();
+                System.out.print("Masukkan pilihan: ");
+                int choice = data1.scanner.nextInt();// user input menu
+                data1.scanner.nextLine();//membersihkan buffer input
+                data1.handleUserInput(choice);//memanggil method handle user dan menjalankan program berdasarkan pilihan user
+
+            } catch (InputMismatchException e) {//Exception jika user input non integer
+                System.err.println("Masukkan pilihan menu dalam bilangan Bulat");
+                data1.scanner.next();//membersihkan buffer input
+            }
         }
     }
 }
